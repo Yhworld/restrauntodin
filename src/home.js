017f01f70b154e1c36e70cf4b2def48e6c158668
Yahya <yahyahussein670@gmail.com>
2023-05-27 09:44:39 +0300

    add navbar

--- src/home.js
+++ src/home.js
@@ -1,5 +1,43 @@
-function test() {
-    return "blah blah blah blah blah blah blah blah blah blah blah"
+function header() {
+  const titlebar = document.createElement("section");
+  titlebar.classList.add("navbar");
+
+  const logo = document.createElement("h1");
+  logo.classList.add("logo");
+  logo.textContent = "Cuisines";
+
+  titlebar.appendChild(logo);
+  titlebar.appendChild(createNavbar());
+  return titlebar;
 }
 
-export default test
+const createNavbar = () => {
+  const nav = document.createElement("nav");
+
+  const homeBtn = document.createElement("button");
+  homeBtn.classList.add("btn-nav");
+  homeBtn.textContent = "Home";
+
+  const menuBtn = document.createElement("button");
+  menuBtn.classList.add("btn-nav");
+  menuBtn.textContent = "Menu";
+
+  const contactBtn = document.createElement("button");
+  contactBtn.classList.add("btn-nav");
+  contactBtn.textContent = "Contact";
+
+  nav.appendChild(homeBtn);
+  nav.appendChild(menuBtn);
+  nav.appendChild(contactBtn);
+
+  return nav;
+};
+
+const initialize = () => {
+  const content = document.querySelector("body");
+  content.appendChild(header());
+
+  return initialize;
+};
+
+export default initialize;
