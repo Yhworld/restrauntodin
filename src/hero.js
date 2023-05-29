cbb8b593eafb34955dcd90f2374b2a76c112dbaa
Yahya <yahyahussein670@gmail.com>
2023-05-29 12:37:25 +0300

    Add home page content

---
+++ src/hero.js
@@ -0,0 +1,35 @@
+import burger from "./assets/burger.png"
+
+
+function createHero() {
+    const hero = document.createElement("section");
+    hero.classList.add("hero");
+
+
+    // const heroImageGroup = document.createElement("div");
+    const heroImage = document.createElement("img");
+    heroImage.src = burger
+
+    // heroImageGroup.appendChild(heroImage);
+
+    hero.innerHTML = `
+    <div class="hero-text">
+    <h2>
+    An Epicurean Journey 
+    <br>
+    Awaits You
+    </h2>
+    <button type="button" class="hero-btn">Get started</button>
+    </div>
+    `
+    hero.appendChild(heroImage);
+    return hero
+}
+
+const renderHero = () => {
+    const main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createHero());
+}
+
+export default renderHero;
