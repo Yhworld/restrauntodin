b1a2296e1104752cc6a320c422ec53cacfbd8b01
Yahya <yahyahussein670@gmail.com>
2023-05-30 17:22:27 +0300

    set active property that highlights the nav

--- src/home.js
+++ src/home.js
@@ -1,10 +1,12 @@
+import renderHero from "./hero";
+
 function header() {
   const titlebar = document.createElement("section");
   titlebar.classList.add("navbar");
 
   const logo = document.createElement("h1");
   logo.classList.add("logo");
-  logo.textContent = "Cuisines";
+  logo.textContent = "Grill Shack.";
 
   titlebar.appendChild(logo);
   titlebar.appendChild(createNavbar());
@@ -17,6 +19,13 @@ const createNavbar = () => {
   const homeBtn = document.createElement("button");
   homeBtn.classList.add("btn-nav");
   homeBtn.textContent = "Home";
+  homeBtn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeBtn);
+    renderHero();
+  });
+
+
 
   const menuBtn = document.createElement("button");
   menuBtn.classList.add("btn-nav");
@@ -33,10 +42,32 @@ const createNavbar = () => {
   return nav;
 };
 
+function createMain() {
+    const main = document.createElement("main");
+    main.classList.add("main");
+    main.setAttribute("id", "main");
+    return main;
+  }
+
+  function setActiveButton(button) {
+    const buttons = document.querySelectorAll(".btn-nav");
+  
+    buttons.forEach((button) => {
+      if (button !== this) {
+        button.classList.remove("active");
+      }
+    });
+  
+    button.classList.add("active");
+  }
+
 const initialize = () => {
   const content = document.querySelector("body");
   content.appendChild(header());
+  content.appendChild(createMain())
 
+  setActiveButton(document.querySelector(".btn-nav"));
+  renderHero();
   return initialize;
 };
 
